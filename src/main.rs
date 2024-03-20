81611f4a6a29cfbe71dd38ed3580a7d101171fbb
ignighttt <guilhermesousa.gs11@gmail.com>
2024-03-20 14:37:42 -0300

    button

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use bevy::{app::{App, Startup, Update}, ecs::{bundle, component::Component, query::With, system::{Commands, Query}}, scene::ron::Number, DefaultPlugins};
+use bevy::{
+    app::{App, Startup, Update},
+    ecs::{
+        bundle,
+        component::Component,
+        query::With,
+        system::{Commands, Query},
+    },
+    scene::ron::Number,
+    winit::WinitSettings,
+    DefaultPlugins,
+};
 use game::ClickerGamePlugin;
 
 mod game;
 
 fn main() {
     App::new()
-    .add_plugins((DefaultPlugins, ClickerGamePlugin))
+        .add_plugins((DefaultPlugins, ClickerGamePlugin))
         .run();
 }
